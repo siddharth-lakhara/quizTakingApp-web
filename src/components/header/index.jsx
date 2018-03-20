b796f3a664563c73c0ede783f30c6d74e1f1e116
lakhara <siddharth.lakhara@gmail.com>
2018-03-20 22:42:28 +0530

    header done

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import './header.css';
 
-const Headers = () => (<div>Header here</div>);
+const Headers = props => (
+  <div className="Header-main">
+    <span className="Header-title">Quizzy</span>
+    <span className="Header-userWelcome" style={{ display: props.user ? 'inline' : 'none' }}>Hello {props.user}!</span>
+  </div>
+);
 
 export default Headers;
